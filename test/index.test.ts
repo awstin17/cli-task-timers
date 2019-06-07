1a7165ca3fc620bd199d8dd9e65762cbf6ae18d4
awstin17 <austinwiese.acw@gmail.com>
2019-06-07 14:18:40 -0700

    Fixed some formatting errors. First edits to test file

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,28 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
+const taskGather = require('../src/prompter')
 
 import cmd = require('../src')
 
-describe('TaskTimer', () => {
-  test
-    .stdout()
-    .do(() => cmd.run([]))
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
 
-  test
-    .stdout()
-    .do(() => cmd.run(['--name', 'jeff']))
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
+describe('TaskGatherFunction', () => {
+
+  test.it('returns an array of tasks!', () => {
+    expect(taskGather().then((res: any) => { return res })).to.be.an('array')
+  })
 })
+
+// describe('TaskTimer', () => {
+//   test
+//     .stdout()
+//     .do(() => cmd.run([]))
+//     .it('runs hello', ctx => {
+//       expect(ctx.stdout).to.contain('hello world')
+//     })
+
+//   test
+//     .stdout()
+//     .do(() => cmd.run(['--name', 'jeff']))
+//     .it('runs hello --name jeff', ctx => {
+//       expect(ctx.stdout).to.contain('hello jeff')
+//     })
+// })
