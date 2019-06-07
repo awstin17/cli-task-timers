f37871744faaffcafba39d81bb55fa4eb1209bee
awstin17 <austinwiese.acw@gmail.com>
2019-06-07 07:51:07 -0700

    combined start first task and start rest of task functions to just one function in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import { Command } from "@oclif/command";
-const alerts = require('./notifications');
-const gatherTasks = require('./prompter');
-const startTimer = require('./timer');
+const alerts = require('./notifications'); // imports modified node-notifier object
+const gatherTasks = require('./prompter'); // imports function that captures user input
+const startTimer = require('./timer'); // imports function that starts a timer
 
 class TaskTimer extends Command {
 
-  async startFirstTask(task: any, time: number) {
-    alerts.displayFirstMessage(task, time)
-    await startTimer(time * 60000);
-  }
+  async doTasks(tasks: Array<any>) {
 
-  async startRestOfTasks(tasks: Array<any>) {
+    // First Task
+    alerts.displayFirstMessage(tasks[0].task, tasks[0].time)
+    await startTimer(tasks[0].time * 60000);
+
+    //Middle tasks to last task
     for (let j = 1; j < tasks.length; j++) {
-      let { task, time } = tasks[j];
-      alerts.displayMiddleMessage(task, time);
-      await startTimer(time * 60000);
+      alerts.displayMiddleMessage(tasks[j].task, tasks[j].time);
+      await startTimer(tasks[j].time * 60000);
     }
+
+    //Ending Message
     alerts.displayLastMessage();
   }
 
   async run() { //Two step tool
     let tasks = await gatherTasks(); // 1) Gather tasks from user and time they want to spend on them
-    await this.startFirstTask(tasks[0].task, tasks[0].time)
-    await this.startRestOfTasks(tasks)
+    await this.doTasks(tasks) // 2) Go through task list, display message, then start timer
   }
 }
 
